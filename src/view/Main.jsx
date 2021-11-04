eae6337bd88e514867d747674e476e9eb3fa405d
milkness <dfhilder@gmail.com>
2021-11-05 00:22:15 +0900

    add dropdown, write button

--- src/view/Main.jsx
+++ src/view/Main.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import MultipleTabBar from "../components/MultipleTabBar";
 import Navbar from "../components/Navbar";
 import TabBar from "../components/TabBar";
 import TextTabBar from "../components/TextTabBar";
+
+import styles from "./main.module.scss";
 
 const tabs = ["모집", "초대"];
 const secondTabs = ["지역", "대회분류"];
@@ -33,16 +35,30 @@ const contestCategoryTabs = [
 ];
 
 const Main = () => {
-  const [selectedTab, setSelectedTab] = useState("모집");
-  const [selectedSecondTab, setSelectedSecondTab] = useState("지역");
-  const [selectedThirdTab, setSelectedThirdTab] = useState([]);
+  const [selectedTab, setSelectedTab] = useState(tabs[0]);
+  const [selectedSecondTab, setSelectedSecondTab] = useState(secondTabs[0]);
+  const [selectedThirdTab, setSelectedThirdTab] = useState([localTabs[0]]);
+
+  useEffect(() => {
+    
+  },[])
 
   function changeTab(t) {
     setSelectedTab(t);
+    resetThirdTab(t);
   }
   function changeSecondTab(t) {
     setSelectedSecondTab(t);
+    resetThirdTab(t);
   }
+  
+  function resetThirdTab(t) {
+    let temp = selectedThirdTab;
+    temp = temp.filter((e) => false)
+    let defaultElement = t === "지역" ? localTabs[0] : contestCategoryTabs[0];
+    setSelectedThirdTab([...temp, defaultElement]);
+  }
+
   function changeThirdTab(t) {
     let temp = selectedThirdTab;
     if (!selectedThirdTab.includes(t)) {
@@ -70,11 +86,20 @@ const Main = () => {
           callback={(t) => changeSecondTab(t)}
         ></TextTabBar>
       </div>
-      <MultipleTabBar
+      <MultipleTabBar className="3rdTabBar"
         tabs={selectedSecondTab === "지역" ? localTabs : contestCategoryTabs}
         selected={selectedThirdTab}
         callback={(t) => changeThirdTab(t)}
       ></MultipleTabBar>
+      <div className={styles.dropdownContainer}>
+       <select name="area">
+          <option value="최신순" disabled>
+            최신순
+          </option>
+          {/* 지역 셀렉트 */}
+        </select>
+        <button className={`${styles.btnMain} btn-main`}>글쓰기</button>
+        </div>
     </div>
   );
 };
