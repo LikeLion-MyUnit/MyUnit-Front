be947b2ef4a11ccb600722847ef45253e6c81ea3
pitterpark <dfhilder@gmail.com>
2021-11-14 15:47:22 +0900

    Fix : sign up format

--- src/service/BoardService.jsx
+++ src/service/BoardService.jsx
@@ -1,29 +1,29 @@
 import axios from "axios";
 import { serverURL } from "./ServerConst";
 
-export async function requestSignup(id, password, nickname) {
-  try {
-    let response = await axios.post(
-      `${serverURL}/account/user/signup/`,
+// export async function requestSignup(id, password, nickname) {
+//   try {
+//     let response = await axios.post(
+//       `${serverURL}/account/user/signup/`,
 
-      {
-        nickname: nickname,
-        email: id,
-        password: password,
-      }
-    );
-    response.data.password = "";
-    console.log(response.data);
-    return response.data;
-  } catch (e) {
-    console.log(e.response.data);
-    if (Object.keys(e.response.data).includes("email")) {
-      return "중복된 이메일이 존재합니다.";
-    } else if (Object.keys(e.response.data).includes("nickname")) {
-      return "중복된 닉네임이 존재합니다.";
-    } else {
-      return "예기치 못한 에러가 발생했습니다.";
-    }
-    //TODO : save to cookie
-  }
-}
+//       {
+//         nickname: nickname,
+//         email: id,
+//         password: password,
+//       }
+//     );
+//     response.data.password = "";
+//     console.log(response.data);
+//     return response.data;
+//   } catch (e) {
+//     console.log(e.response.data);
+//     if (Object.keys(e.response.data).includes("email")) {
+//       return "중복된 이메일이 존재합니다.";
+//     } else if (Object.keys(e.response.data).includes("nickname")) {
+//       return "중복된 닉네임이 존재합니다.";
+//     } else {
+//       return "예기치 못한 에러가 발생했습니다.";
+//     }
+//     //TODO : save to cookie
+//   }
+// }
--- src/service/ServerConst.jsx
+++ src/service/ServerConst.jsx
@@ -1,3 +1,4 @@
 // Dependency Injection을 위한 파일
 
-export const serverURL = "http://127.0.0.1:8000";
+export const serverURL =
+  "http://681b-2001-4430-c0d1-98f4-e113-5df7-3676-49be.ngrok.io";
