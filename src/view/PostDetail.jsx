e0add802b10160a8bc2d955c96ac3a40573e77ea
Jsgithubchannel <jsschannel99@gmail.com>
2021-11-21 18:24:09 +0900

    add poster image onclick event

--- src/view/PostDetail.jsx
+++ src/view/PostDetail.jsx
@@ -1,38 +1,41 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import styles from "./PostDetail.module.scss";
 import "../common/Common.scss";
-import { serverURL } from '../service/ServerConst';
-import axios from 'axios'
+import { serverURL } from "../service/ServerConst";
+import axios from "axios";
 
 function PostDetail(props) {
-    const {history,location} = props;
-    const image = `${serverURL}/board${location.state.image}`
-    return (
-        <div>
-            <Navbar/>
-            <div className={styles.container}>
-                <div className={styles.header}>
-                    <div className="">대회명:{location.state.title}</div>
-                    <div className="">작성자: {location.state.author}</div>
-                    <div className="">대회명:{location.state.contest}</div>
-                    <hr/>
-                </div>
-                <div className="content">
-                    {location.state.content}
-                </div>
-                <hr/>
-                <div className={styles.image_container}>
-                    <div className={styles.image}>
-                        <img src={image}/>
-                    </div>
-                </div>
-                <div className={styles.button_container}>
-                <button className="btn-main">메세지 보내기</button>
-                </div>
-            </div>
-        </div>
-    )
+  const { history, location } = props;
+  const image = `${serverURL}/board${location.state.image}`;
+
+  const [imageClick, setImageClick] = useState(false);
+
+  const handleImageToggle = () => {
+    setImageClick(!imageClick);
+  };
+
+  return (
+    <div className={styles.container}>
+      <Navbar />
+      <div className={styles.header}>
+        <div className="">대회명:{location.state.title}</div>
+        <div className="">작성자: {location.state.author}</div>
+        <div className="">대회명:{location.state.contest}</div>
+        <hr />
+      </div>
+      <div className="content">{location.state.content}</div>
+      <hr />
+      <img
+        src={image}
+        className={imageClick ? styles.imageOnClick : styles.image}
+        onClick={handleImageToggle}
+      />
+      <div className={styles.button_container}>
+        <button className="btn-main">메세지 보내기</button>
+      </div>
+    </div>
+  );
 }
 
-export default PostDetail
+export default PostDetail;
