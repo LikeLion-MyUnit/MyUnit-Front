c0f3b3216535bfa33f420b033d0e7bddf0ae6cae
milkness <dfhilder@gmail.com>
2022-04-25 00:36:24 +0900

    Fix: uncorrected elements

--- src/components/UserInfoBtns.jsx
+++ src/components/UserInfoBtns.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import styles from "./UserInfoBtns.module.scss";
 
-const UserInfoBtns = ({ details }) => {
+const UserInfoBtns = ({ gender, city, interest }) => {
   return (
     <div className={styles.infoBtns}>
-      <button className={`${styles.btn} btn-main`}>{details.gender}</button>
-      <button className={`${styles.btn} btn-main`}>{details.city}</button>
-      <button className={`${styles.btn} btn-main`}>{details.interest}</button>
+      <button className={`${styles.btn} btn-main`}>{gender}</button>
+      <button className={`${styles.btn} btn-main`}>{city}</button>
+      <button className={`${styles.btn} btn-main`}>{interest}</button>
     </div>
   );
 };
